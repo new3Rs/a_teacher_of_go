73e4190e69469ff9cb64b0870e627b95cb10a171
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2020-06-03 16:05:39 +0900

    docs

---
+++ docs/pre_pre.js
@@ -0,0 +1,111 @@
+/**
+ * @preserve Copyright 2020 ICHIKAWA, Yuji (New 3 Rs)
+ */
+
+ // status-  1: ready, -1: fail
+function katagoStatusHandler(status) {
+    const command = document.getElementById("input").command
+    switch (status) {
+        case 1:
+        command.removeAttribute("disabled");
+        command.setAttribute("placeholder", "Input a GTP command");
+        if (window.controller) {
+            window.controller.onReady();
+        }
+        break;
+        case -1:
+        command.setAttribute("placeholder", "Engine failed loading a weight");
+        alert("ニューラルネットワークのウェイトをロードできませんでした");
+        break;
+        default:
+        console.log("should not reach here");
+        break;
+    }
+}
+
+class Input {
+    constructor() {
+        this.buffer = "";
+
+        document.getElementById("input").addEventListener("submit", event => {
+            event.preventDefault();
+            this.buffer += event.currentTarget.command.value + "\n";
+            document.getElementById("log").value += event.currentTarget.command.value + "\n";
+            event.currentTarget.command.value = "";
+            if (this.resolve) {
+                this.resolve();
+            }
+        }, false);
+    }
+
+    callback() {
+        if (!this.buffer) {
+            return null;
+        }
+        const c = this.buffer[0];
+        this.buffer = this.buffer.substr(1);
+        return c.charCodeAt(0);
+    }
+    
+    wait() {
+        return new Promise((res, rej) => {
+            this.resolve = res;
+            this.reject = rej;
+        });
+    }
+}
+
+class Output {
+    constructor() {
+        this.buffer = "";
+    }
+
+    callback(char) {
+        switch (char) {
+            case 0: // NULL
+            case 10: // "\n"
+            const output = document.getElementById("output")
+            output.value += this.buffer;
+            document.getElementById("log").value += this.buffer + "\n";
+            output.dispatchEvent(new CustomEvent("message"));
+            this.buffer = "";
+            break;
+            case 13: // "\r"
+            break;
+            default:
+            this.buffer += String.fromCharCode(char);
+        }
+    }
+}
+
+if (typeof Module === "undefined") {
+    Module = {};
+}
+if (!("arguments" in Module)) {
+    Module["arguments"] = [];
+}
+if (!("preRun" in Module)) {
+    Module["preRun"] = [];
+}
+Module["preRun"].push(function() {
+    const params = new URL(location).searchParams;
+    const cfgFile = params.get("config") || "gtp_auto.cfg";
+    FS.createPreloadedFile(
+        FS.cwd(),
+        cfgFile,
+        cfgFile,
+        true, // 読み込み許可
+        false // 書き込み許可
+    );
+    Module["arguments"].push(params.get("subcommand") || "gtp");
+    Module["arguments"].push("-model");
+    Module["arguments"].push(params.get("model") || "web_model");
+    Module["arguments"].push("-config");
+    Module["arguments"].push(cfgFile);
+
+    const input = new Input();
+    const output = new Output();
+    FS.init(input.callback.bind(input), output.callback.bind(output), null);
+    Module["input"] = input;
+    Module["output"] = output;
+});
---
+++ docs/precache-manifest.b06d63744503fa2cf117be0b68a29272.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2676aca3a226bf77fc05abbc2aebd01",
+    "url": "/a_teacher_of_go/index.html"
+  },
+  {
+    "revision": "2f8205725b44ab0b6d50",
+    "url": "/a_teacher_of_go/static/css/main.14a879dd.chunk.css"
+  },
+  {
+    "revision": "1eb60c7a5dd9aafc9d4e",
+    "url": "/a_teacher_of_go/static/js/2.4b970e97.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/a_teacher_of_go/static/js/2.4b970e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f8205725b44ab0b6d50",
+    "url": "/a_teacher_of_go/static/js/main.e970637a.chunk.js"
+  },
+  {
+    "revision": "1b35684b6adc53801cdf66ccb50d9350",
+    "url": "/a_teacher_of_go/static/js/main.e970637a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21ab98058c95a67bad8",
+    "url": "/a_teacher_of_go/static/js/runtime-main.9e738a7d.js"
+  }
+]);
